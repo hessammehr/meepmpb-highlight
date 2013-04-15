329e233b5a3c3c80ba60ffa6e10ef55771b37ed8
Hessam Mehr <hessammehr@gmail.com>
2013-04-14 19:00:53 -0700

    Installation updated for GtkSourceView 3

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
-cp ctl.lang /usr/share/gtksourceview-2.0/language-specs
+SOURCEVIEW_3_DIR=/usr/share/gtksourceview-3.0/language-specs
+SOURCEVIEW_2_DIR=/usr/share/gtksourceview-2.0/language-specs
+
+if [ -d "$SOURCEVIEW_3_DIR" ] then
+	cp ctl.lang $SOURCEVIEW_3_DIR
+fi
+
+if [ -d "$SOURCEVIEW_2_DIR" ] then
+	cp ctl.lang $SOURCEVIEW_2_DIR
+fi
+
 cp x-meepmpb.xml /usr/share/mime/packages
 update-mime-database /usr/share/mime
